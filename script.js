5043d226a5d015ca21d974fe8b711d3c65f73eed
jagdish1992max <jagdishjd6@gmail.com>
2026-04-20 14:27:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+async function generateTitle(){
 
+let topic=document.getElementById("topic").value
+
+let result=await askAI("Give a viral YouTube title about "+topic)
+
+document.getElementById("result").innerText=result
+
+}
+
+async function generateScript(){
+
+let topic=document.getElementById("topic").value
+
+let result=await askAI("Write a YouTube Shorts script about "+topic)
+
+document.getElementById("result").innerText=result
+
+}
+
+async function generateHook(){
+
+let topic=document.getElementById("topic").value
+
+let result=await askAI("Give a viral hook line for "+topic)
+
+document.getElementById("result").innerText=result
+
+}
+
+async function generateIdea(){
+
+let topic=document.getElementById("topic").value
+
+let result=await askAI("Give a viral video idea about "+topic)
+
+document.getElementById("result").innerText=result
+
+}
