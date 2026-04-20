76811e202a331bfe4b995c3b5edf47c045dcaea8
jagdish1992max <jagdishjd6@gmail.com>
2026-04-20 15:17:52 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,28 +1,28 @@
-async function askAI(prompt){
-
-let response=await fetch("https://api.openai.com/v1/chat/completions",{
-
-method:"POST",
-
-headers:{
-"Content-Type":"application/json",
-"Authorization":"Bearer AIzaSyAX2Y_c-ryZVAmPe9gLJemoPdQBQCxmtYM"
-},
-
-body:JSON.stringify({
-
-model:"gpt-4o-mini",
-
-messages:[
-{role:"user",content:prompt}
-]
-
-})
-
-})
-
-let data=await response.json()
-
-return data.choices[0].message.content
-
-}
+‎async function askAI(prompt){
+‎
+‎let response=await fetch("https://api.openai.com/v1/chat/completions",{
+‎
+‎method:"POST",
+‎
+‎headers:{
+‎"Content-Type":"application/json",
+‎"Authorization":"Bearer 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"
+‎},
+‎
+‎body:JSON.stringify({
+‎
+‎model:"gpt-4o-mini",
+‎
+‎messages:[
+‎{role:"user",content:prompt}
+‎]
+‎
+‎})
+‎
+‎})
+‎
+‎let data=await response.json()
+‎
+‎return data.choices[0].message.content
+‎
+‎}
